d9b22091d6be3334fd2409280343697b7165da8e
codeamac11 <codeamac11@gmail.com>
2017-01-31 13:31:35 -0600

    file correct

---
+++ pseudocodigo_de_calificaciones.rb
@@ -0,0 +1,42 @@
+#paso uno
+definimos el metodo bets_students
+# paso 2
+asignamos una variable con nombre students que recibe atributos del metodo
+#paso 3
+definimos variable X , Y con con valor a cero
+#paso 4
+mientra el valor de y sea menor que a la longitud de variable students
+    #paso 5
+    guardar en la variable Y , X los valores de la longitud
+    #paso 6 
+    asignar el valor de students con los valores de Y , X en la variable current_student
+    #paso 7
+  SI current_student en indice 1 dentro del array es igual a 10
+    #paso 8
+    Guardar en bets_students el valor del indice 0
+    # paso 9
+  cerrar SI 
+
+    # paso 10
+  SI la longitud de students con indice Y menos 1 es igual a el valor X
+    # paso 11
+    definimos a X con valor a 0
+    # paso 12
+    definimos a Y + 1 realizando el incremento
+    # paso 13
+    de lo contrario, si no se cumple que increnete X mas 1
+    # paso 14
+  cierre SI
+  # paso 15
+cerrar mientras
+# paso 16
+regresamos el array bets_students con el resultado de las condiciones
+# paso 17
+cierre de Metodo
+
+# 
+Asignamos un array  de donde tomamos los parametro 
+con nombre students
+
+# paso 19
+definimos una prueba para comprobar el resultado True
